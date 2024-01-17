3e0326c84d9d2953f1cb74b4d77193adbf953d55
MariiaRev <maria.revenko2209@gmail.com>
2024-01-17 22:17:06 +0200

    added services config

---
+++ Rainfall.Core/ConfigurationServices.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Rainfall.Core.FloodMonitoring;
+
+namespace Rainfall.Core
+{
+    public static class ConfigurationServices
+    {
+        public static IServiceCollection RegisterCoreServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IFloodMonitoringService, FloodMonitoringService>();
+            return services;
+        }
+    }
+}
