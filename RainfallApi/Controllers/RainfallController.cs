193d88e17187fef06f48f942f584df91021e6541
MariiaRev <maria.revenko2209@gmail.com>
2024-01-17 22:21:58 +0200

    added rainfall contraller and set swagger

---
+++ RainfallApi/Controllers/RainfallController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rainfall.ApiModels;
+using Rainfall.Core.FloodMonitoring;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+
+namespace RainfallApi.Controllers
+{
+    /// <summary>
+    /// Operations relating to rainfall
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class RainfallController : Controller
+    {
+        private readonly IFloodMonitoringService _floodMonitoringService;
+
+        /// <summary>
+        /// Rainfall controller's constructor
+        /// </summary>
+        /// <param name="floodMonitoringService"></param>
+        public RainfallController(IFloodMonitoringService floodMonitoringService)
+        {
+            _floodMonitoringService = floodMonitoringService;
+        }
+
+        /// <summary>
+        /// Get rainfall readings by station Id
+        /// </summary>
+        /// <remarks>Retrieve the latest readings for the specified stationId</remarks>
+        /// <param name="stationId">The id of the reading station</param>
+        /// <param name="count">The number of readings to return</param>
+        /// <response code="200">A list of rainfall readings successfully retrieved</response>
+        /// <response code="400">Invalid request</response>
+        /// <response code="404">No readings found for the specified stationId</response>
+        /// <response code="500">Internal server error</response>
+        [HttpGet]
+        [Route("id/{stationId}/readings")]
+        [Tags("Rainfall")]
+        [EndpointDescription("Retrieve the latest readings for the specified stationId")]
+        [ProducesResponseType(typeof(RainfallReadingResponse), 200)]
+        [ProducesResponseType(typeof(Error), 400)]
+        [ProducesResponseType(typeof(Error), 404)]
+        [ProducesResponseType(typeof(Error), 500)]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetRainfall([NotNull] string stationId, [Range(1, 100)] int count = 10)
+        {
+            try
+            {
+                var (statusCode, result) = await _floodMonitoringService.Get(stationId, count);
+
+                return statusCode switch
+                {
+                    HttpStatusCode.OK => Ok(result),
+                    HttpStatusCode.BadRequest => BadRequest(result),
+                    HttpStatusCode.NotFound => NotFound(result),
+                    _ => InternalServerError(result),
+                };
+            }
+            catch(Exception ex)
+            {
+                Error error = new() { Message = ex.Message, Detail = new() };
+                return InternalServerError(error);
+            }
+        }
+
+        private static ObjectResult InternalServerError(IResponse response)
+        {
+            return new ObjectResult(response) { StatusCode = (int)HttpStatusCode.InternalServerError };
+        }
+    }
+}
--- RainfallApi/Program.cs
+++ RainfallApi/Program.cs
@@ -1,11 +1,48 @@
+using Microsoft.OpenApi.Models;
+using Rainfall.Core;
+using System.Reflection;
+using File = System.IO.File;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "1.0",
+        Title = "Rainfall Api",
+        Description = "An API which provides rainfall reading data"
+    });
+
+    options.AddServer(new OpenApiServer()
+    {
+        Url = "http://localhost:3000",
+        Description = "Rainfall Api",
+    });
+
+    options.CustomOperationIds(e => $"{e.HttpMethod}-{e.ActionDescriptor.RouteValues["controller"]}".ToLower());
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    if (File.Exists(xmlPath))
+    {
+        options.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+        options.EnableAnnotations();
+    }
+});
+
+builder.Services.AddHttpClient("rainfallClient", client =>
+{
+    client.BaseAddress = new Uri("http://environment.data.gov.uk");                     // TODO: move and take from appsettings
+});
+
+builder.Services.RegisterCoreServices();
 
 var app = builder.Build();
 
@@ -13,11 +50,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.yaml", "v1");
+    });
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
